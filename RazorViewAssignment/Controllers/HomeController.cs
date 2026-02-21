50f3d3f95b7c789be19642e685cf0745b9344a4a
Mahbub Ali Arju <mahbub.arju@bracits.com>
2026-02-22 01:30:20 +0600

    weather app project to demonstrate razor view

---
+++ RazorViewAssignment/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorViewAssignment.Models;
+using RazorViewAssignment.Services;
+
+namespace RazorViewAssignment.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        private readonly WeatherData _weatherData = new WeatherData();
+        [Route("/")]
+        public IActionResult AllCityWeather()
+        {
+            return View(_weatherData.getAllData());
+        }
+        [Route("/weather/{cityCode?}")]
+        public IActionResult WetherDetails(string? cityCode)
+        {
+            CityWeather? matchingData = _weatherData.getAllData().FirstOrDefault(x => x.CityUniqueCode == cityCode);
+
+            if(matchingData is null)
+            {
+                return View("NotFoundPage");
+            }
+            else
+            {
+                return View(matchingData);
+            }
+        }
+    }
+}
---
+++ RazorViewAssignment/Services/WeatherData.cs
@@ -0,0 +1,40 @@
+﻿using RazorViewAssignment.Models;
+
+namespace RazorViewAssignment.Services
+{
+    public class WeatherData
+    {
+        List<CityWeather> cityWeathers = new List<CityWeather>()
+{
+    new CityWeather() {
+        CityUniqueCode = "LDN",
+        CityName = "London",
+        DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
+        TemperatureFahrenheit = 33
+    },
+    new CityWeather() {
+        CityUniqueCode = "NYC",
+        CityName = "New York", // Fixed from "London" in your snippet
+        DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
+        TemperatureFahrenheit = 60
+    },
+    new CityWeather() {
+        CityUniqueCode = "PAR",
+        CityName = "Paris",
+        DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
+        TemperatureFahrenheit = 82
+    }
+};
+        public List<CityWeather> getAllData()
+        {
+            return cityWeathers;
+        }
+        public CityWeather? getByCode(string code)
+        {
+            CityWeather matchingWeather = cityWeathers.FirstOrDefault(x => x.CityUniqueCode == code);
+            return matchingWeather;
+        
+        }
+
+    }
+}
