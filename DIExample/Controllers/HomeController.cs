2599ecfc596dc6db38a9231cca2b1e84593dbb85
Mahbub Ali Arju <mahbub.arju@bracits.com>
2026-03-08 22:16:37 +0600

    practicing razor views

---
+++ DIExample/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+namespace DIExample.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        private readonly CitiesService _cityService;
+        public HomeController()
+        {
+            _cityService = new CitiesService();
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(_cityService.getCities());
+        }
+    }
+}
---
+++ Services/CitiesService.cs
@@ -0,0 +1,14 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> cities = new List<string>();
+        public CitiesService() {
+            cities = ["Dhaka", "Noakhali", "Comilla"];
+        }
+        public List<string> getCities()
+        {
+            return cities;
+        }
+    }
+}
